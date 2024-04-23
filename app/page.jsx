7be08cc6052fab5d3f9a71338de7fac555987a77
cristianmihai01 <dev.cristianmihai@gmail.com>
2024-04-23 12:51:27 +0300

    added

--- app/page.jsx
+++ app/page.jsx
@@ -12,15 +12,14 @@ const Home = () => {
           {/* text */}
           <div className='order-2 xl:order-none text-center xl:text-left'>
             <span>Software Developer</span>
-            <h1 className='h1 mb-8'>
+            <h1 className='h1 mb-6'>
               Hello I’m
               <br />
               <span className='text-accent'>Luke Coleman</span>
             </h1>
-            <p className='max-w-[512px] mb-8'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Scelerisque consequat, faucibus et, et. Non semper blandit vitae
-              semper blandit.
+            <p className='max-w-[500px] mb-9 text-white/80'>
+              I excel at crafting elegant digital experiences and am proficient
+              in various programming languages and technologies.
             </p>
             <div className='flex flex-col xl:flex-row items-center gap-8'>
               <Button
--- components/Stats.jsx
+++ components/Stats.jsx
@@ -43,7 +43,7 @@ const Stats = () => {
                 <p
                   className={`${
                     item.text.length < 15 ? 'max-w-[100px]' : 'max-w-[150px]'
-                  } leading-snug`}
+                  } leading-snug text-white/80`}
                 >
                   {item.text}
                 </p>

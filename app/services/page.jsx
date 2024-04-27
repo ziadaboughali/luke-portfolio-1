052ace4bf8707a163631e43a648f9ed6bbf2666c
cristianmihai01 <dev.cristianmihai@gmail.com>
2024-04-27 14:06:14 +0300

    added

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,62 +1,38 @@
 'use client';
 
-import { FaLaptopCode, FaCogs } from 'react-icons/fa';
-import { RiPencilRuler2Fill } from 'react-icons/ri';
-import { BiSolidVector } from 'react-icons/bi';
-import { FaRankingStar } from 'react-icons/fa6';
-import { IoMegaphone } from 'react-icons/io5';
-import { FaArrowRight } from 'react-icons/fa6';
+import { BsArrowDownRight } from 'react-icons/bs';
 
 import Link from 'next/link';
 
 const services = [
   {
-    icon: <RiPencilRuler2Fill />,
-    title: 'Web Design',
-    description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit ipsa dolores.',
-    linkText: 'Read more',
-    linkPath: '',
-  },
-  {
-    icon: <FaLaptopCode />,
+    num: '01',
     title: 'Web Development',
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit ipsa dolores.',
-    linkText: 'Read more',
-    linkPath: '',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque consequat, faucibus et, et.',
+    href: '',
   },
   {
-    icon: <FaCogs />,
-    title: 'App Development',
+    num: '02',
+    title: 'UX/UX Design',
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit ipsa dolores.',
-    linkText: 'Read more',
-    linkPath: '',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque consequat, faucibus et, et.',
+    href: '',
   },
   {
-    icon: <BiSolidVector />,
+    num: '03',
     title: 'Logo Design',
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit ipsa dolores.',
-    linkText: 'Read more',
-    linkPath: '',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque consequat, faucibus et, et.',
+    href: '',
   },
+
   {
-    icon: <FaRankingStar />,
+    num: '04',
     title: 'SEO',
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit ipsa dolores.',
-    linkText: 'Read more',
-    linkPath: '',
-  },
-  {
-    icon: <IoMegaphone />,
-    title: 'Marketing',
-    description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit ipsa dolores.',
-    linkText: 'Read more',
-    linkPath: '',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque consequat, faucibus et, et.',
+    href: '',
   },
 ];
 
@@ -64,11 +40,11 @@ import { motion } from 'framer-motion';
 
 const Services = () => {
   return (
-    <section className='pt-12 pb-24'>
+    <section className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
       <div className='container mx-auto'>
-        <h2 className='h2 text-center mb-12'>
+        {/* <h2 className='h2 text-center xl:text-left mb-8 xl:mb-16'>
           My Services<span className='text-accent'>.</span>
-        </h2>
+        </h2> */}
 
         {/* grid */}
         <motion.div
@@ -77,26 +53,30 @@ const Services = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },
           }}
-          className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-[30px]'
+          className='grid grid-cols-1 md:grid-cols-2 gap-[60px]'
         >
           {services.map((service, index) => {
             return (
               <div
                 key={index}
-                className='flex-1 border border-white/10 min-h-[400px] flex flex-col justify-center items-center gap-6 p-6 rounded-2xl hover:bg-white/5 transition-all relative'
+                className='flex-1 flex flex-col justify-center group gap-6'
               >
-                <div className=' bg-white/5 backdrop-blur-3xl w-[92px] h-[92px] flex justify-center items-center rounded-full mb-4'>
-                  <div className='text-[44px] text-accent'>{service.icon}</div>
+                <div className='w-full flex justify-between items-center'>
+                  <div className='text-5xl font-extrabold text-transparent text-outline group-hover:text-outline-hover transition-all duration-500'>
+                    {service.num}
+                  </div>
+                  <Link
+                    href={service.href}
+                    className='w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'
+                  >
+                    <BsArrowDownRight className='text-primary text-3xl' />
+                  </Link>
                 </div>
-                <h3 className='h3'>{service.title}</h3>
-                <p className='text-center'>{service.description}</p>
-                <Link
-                  href={service.linkPath}
-                  className='flex items-center gap-2 hover:gap-4 hover:text-accent transition-all'
-                >
-                  {service.linkText}
-                  <FaArrowRight className='2xl text-accent' />
-                </Link>
+                <h2 className='h2 text-white group-hover:text-accent transition-all duration-500'>
+                  {service.title}
+                </h2>
+                <p className='text-white/60'>{service.description}</p>
+                <div className='w-full border-b border-white/20'></div>
               </div>
             );
           })}
--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,87 @@
+'use client';
+
+// import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import swiper styles
+import 'swiper/css';
+
+import WorkSliderBtns from '@/components/WorkSliderBtns';
+
+// import motion
+import { motion } from 'framer-motion';
+
+const projects = [
+  {
+    title: 'project 1',
+    category: '',
+    links: [
+      {
+        icon: 'github',
+        path: 'http://www.github.com',
+      },
+    ],
+  },
+  {
+    title: 'project 2',
+    category: '',
+    links: [
+      {
+        icon: 'github',
+        path: 'http://www.github.com',
+      },
+    ],
+  },
+  {
+    title: 'project 3',
+    category: '',
+    links: [
+      {
+        icon: 'github',
+        path: 'http://www.github.com',
+      },
+    ],
+  },
+];
+
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },
+      }}
+      className='relative'
+    >
+      <div className='container mx-auto'>
+        <div className='flex gap-8'>
+          <div className='w-[35%] h-[400px] flex flex-col justify-between'>
+            <h3 className='uppercase text-6xl font-bold'>My latest projects</h3>
+            <div className='bg-accent h-[50%] p-4'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div className='w-[65%]'>
+            <Swiper spaceBetween={30} slidesPerView={2} className='h-[500px]'>
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide className='w-full' key={index}>
+                    <div className='h-[400px] bg-white/10'>{project.title}</div>
+                  </SwiperSlide>
+                );
+              })}
+              {/* check with shad cn carousel */}
+              <WorkSliderBtns
+                containerStyles='flex gap-1 absolute left-0 bottom-0 z-20'
+                btnStyles='bg-accent text-primary w-[40px] h-[40px] flex justify-center items-center'
+              />
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Work;

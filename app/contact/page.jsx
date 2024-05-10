8ba51d10fa369da35ce45f1b425c44a79d140554
cristianmihai01 <dev.cristianmihai@gmail.com>
2024-05-10 17:36:54 +0300

    added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
-
 import {
   Select,
   SelectContent,
@@ -12,14 +11,34 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: 'Phone',
+    description: '(+40) 321 654 876',
+  },
+  {
+    icon: <FaEnvelope />,
+    title: 'Email',
+    description: 'youremail@email.com',
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: 'Address',
+    description: 'Code Corner, Tech Town 13579',
+  },
+];
+
 const Contact = () => {
   return (
     <section className='py-12'>
       <div className='container mx-auto'>
-        <div className='flex flex-col lg:flex-row gap-[30px]'>
+        <div className='flex flex-col xl:flex-row gap-[30px]'>
           <div className='xl:w-[54%]'>
             <form className='flex flex-col gap-6 px-10 py-12 bg-[#27272C] rounded-xl'>
-              <h3 className='text-4xl font-bold'>Let's work together</h3>
+              <h3 className='text-4xl text-accent'>Let's work together</h3>
               <p className='text-white/60'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Nesciunt sit illo esse commodi.
@@ -52,7 +71,23 @@ const Contact = () => {
               </Button>
             </form>
           </div>
-          <div className='flex-1'></div>
+          <div className='flex-1 flex items-center xl:justify-end'>
+            <ul className='flex flex-col gap-10'>
+              {info.map((item, index) => {
+                return (
+                  <li className='flex items-center gap-6' key={index}>
+                    <div className='w-[72px] h-[72px] bg-[#27272C] text-accent rounded-md flex items-center justify-center'>
+                      <div className='text-[28px]'>{item.icon}</div>
+                    </div>
+                    <div className='flex-1'>
+                      <p className='text-white/60'>{item.title}</p>
+                      <h3 className='text-xl'>{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </section>

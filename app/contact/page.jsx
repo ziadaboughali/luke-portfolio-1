f0b94a524d1c50302a20933a945e54a02be38316
cristianmihai01 <dev.cristianmihai@gmail.com>
2024-05-10 20:42:43 +0300

    added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
     >
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-[30px]'>
-          <div className='xl:w-[54%]'>
+          <div className='xl:w-[54%] order-2 xl:order-none'>
             <form className='flex flex-col gap-6 p-10 bg-[#27272C] rounded-xl'>
               <h3 className='text-4xl text-accent'>Let's work together</h3>
               <p className='text-white/60'>
@@ -82,12 +82,12 @@ const Contact = () => {
               </Button>
             </form>
           </div>
-          <div className='flex-1 flex items-center xl:justify-end'>
+          <div className='flex-1 flex items-center xl:justify-end xl:order-none mb-8 xl:mb-0'>
             <ul className='flex flex-col gap-10'>
               {info.map((item, index) => {
                 return (
                   <li className='flex items-center gap-6' key={index}>
-                    <div className='w-[72px] h-[72px] bg-[#27272C] text-accent rounded-md flex items-center justify-center'>
+                    <div className='w-[52px] h-[52px] xl:w-[72px]  xl:h-[72px] bg-[#27272C] text-accent rounded-md flex items-center justify-center'>
                       <div className='text-[28px]'>{item.icon}</div>
                     </div>
                     <div className='flex-1'>
